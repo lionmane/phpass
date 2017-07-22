078aad069fcaef2d852de76868d6547bd2d632cc
Mario J. Wunderlich <mario@lionmane.io>
2017-07-22 15:16:11 -0600

    - Added ability to verify data with a regex expression - Added url to articles - Created function get_data that scrapes all

--- src/webscraper.php
+++ src/webscraper.php
@@ -55,6 +55,8 @@
       }
 
       /**
+       * Get the content at the specified URI, or the homepage if no URI is given.
+       *
        * @param bool $uri
        * @return string
        */
@@ -70,10 +72,15 @@
           return $body->getContents();
       }
 
+      /**
+       * Gets all the links in the homepage.
+       *
+       * @param $content
+       * @return array
+       */
       public function get_article_links($content)
       {
           $crawler = new Crawler($content);
-          $results = [];
 
           $trending_links_filter = $crawler->filter('#section-3 .records li');
           $trending_links = $this->get_links_from_filter($trending_links_filter);
@@ -84,6 +91,11 @@
           return array_unique(array_merge($trending_links, $article_links));
       }
 
+      /**
+       * Given a filter with appropriate selectors, gets & returns the lisk of links.
+       * @param $filter
+       * @return array
+       */
       protected function get_links_from_filter($filter)
       {
           $results = [];
@@ -94,23 +106,10 @@
           return $results;
       }
 
-      function scrape_main_site($content)
-      {
-          $crawler = new Crawler($content);
-          $filter = $crawler->filter('.box1 .records.secondary .record');
-          $results = [];
-
-          foreach ($filter as $i => $content) {
-              $content_crawler = new Crawler($content);
-              $results[] = [
-                  'title' => $content_crawler->filter('h2.headline a')->text(),
-                  'subject' => $content_crawler->filter('div.abstract')->text()
-              ];
-          }
-
-          return $results;
-      }
-
+      /**
+       * @param $link
+       * @return array
+       */
       public function scrape_article($link)
       {
           $content = $this->get_content($link);
@@ -124,12 +123,14 @@
               'article.date' => '.meta div.date',
 
               'author.name' => $main_selector . "div.author a",
-              'author.twitter' => [$section_two . "a", 'href'],
+              'author.twitter' => [$section_two . "a", 'href', '/.*twitter.*/'],
               'author.bio' => $section_two,
               'author.url' => [$main_selector . "div.author a", 'href']
           ];
 
-          return $this->parse_selectors($crawler, $selectors);
+          $article = $this->parse_selectors($crawler, $selectors);
+          $article['article']['url'] = $this->target_url . '/' . $link;
+          return $article;
       }
 
       protected function parse_selectors($crawler, $selectors)
@@ -144,40 +145,80 @@
                   $result[$bucket] = [];
 
               $attr = false;
-              if (is_array($selector)) {
+              $regex = false;
+              if (is_array($selector) && count($selector) == 3) {
+                  list($selector, $attr, $regex) = $selector;
+              }
+              elseif (is_array($selector) && count($selector) == 2) {
                   list($selector, $attr) = $selector;
               }
 
-              $result[$bucket][$key] = $this->get_selector_text($crawler, $selector, $attr);
+              $value = $this->get_selector_text($crawler, $selector, $regex, $attr);
+              $result[$bucket][$key] = $value;
           }
 
           return $result;
       }
 
-      function get_selector_text($crawler, $selector, $attr_name=false)
+      function get_selector_text($crawler, $selector, $regex=false, $attr_name=false)
       {
           $filter = $crawler->filter($selector);
           if (!count($filter))
               return null;
           foreach ($filter as $content) {
               if (!$attr_name)
-                  return $content->textContent;
-              return $content->getAttribute($attr_name);
+                  $value = $content->textContent;
+              else
+                  $value = $content->getAttribute($attr_name);
+              if (!$regex)
+                  return $value;
+              elseif ($regex && preg_match($regex, $value))
+                  return $value;
           }
+
+          return null;
+      }
+
+      /**
+       * Gets all the articles data.
+       *
+       * @return array
+       */
+      function get_data()
+      {
+          $content = $this->get_content();
+
+          // Get article links form the main website
+          $links = $this->get_article_links($content);
+          $articles_by_author = [];
+          $ignore_list = ['blog', 'directories'];
+
+          foreach ($links as $link) {
+              // Skip blog entries for now
+              if (preg_match('/.*' . join('|', $ignore_list) . '.*/', $link)) {
+                  continue;
+              }
+
+              $article = $this->scrape_article($link);
+              $author = $article['author']['name'];
+              if (empty($author))
+                  continue;
+
+              if (!array_key_exists($author, $articles_by_author))
+                  $articles_by_author[$author] = [];
+              $articles_by_author[$author][] = $article;
+          }
+
+          return $articles_by_author;
+      }
+
+      function get_author_links($author_url)
+      {
+
       }
 
       function placeholder($someParam)
       {
-          $client = new Client([
-              'base_uri' => 'http://archive-grbj-2.s3-website-us-west-1.amazonaws.com/',
-              'timeout'  => 5.0,
-          ]);
-
-          # Request / or root
-          $response = $client->request('GET', '/');
-          $body = $response->getBody();
-
-          echo $someParam;
       }
   }
 
